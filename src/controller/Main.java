8f16eae7cd7b7b6c4898f5622ee69687fe271d4d
iamm27 <ilhammaziz27@gmail.com>
2014-05-10 00:12:28 +0700

    2stCommit

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -6,8 +6,11 @@
 
 package controller;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JButton;
 
@@ -15,37 +18,76 @@ import javax.swing.JButton;
  *
  * @author Iamm
  */
-public class Main implements ActionListener, Interface{
-    private view.FrmMain view;
-    private model.Main model;
+public class Main implements MouseListener, Interface{
+    public view.FrmDashboard view;
+    public model.Main model;
     
-    public Main(model.Main model, view.FrmMain view){
+    public Main(model.Main model, view.FrmDashboard view){
         this.model=model;
         this.view=view;
-        //view.addListener((ActionListener) this);
-        view.setVisible(true);
+        this.view.addListener(this);
+        this.view.setSize(new Dimension(1024, 576));
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.view.setLocation(dim.width/2-this.view.getSize().width/2, dim.height/2-this.view.getSize().height/2);
     }
     
-    public void actionPerformed(ActionEvent e) {
-        try{            
-            JButton s=(JButton)e.getSource();
-            
-            if(s.getName().equals("btBus")){
-                
-                
-            }
-        }
-        catch(Exception ex){
-            
-        }
-    }
 
-    public view.FrmMain getView() {
+    public view.FrmDashboard getView() {
         return view;
     }
 
-    public void setView(view.FrmMain view) {
+    public void setView(view.FrmDashboard view) {
         this.view = view;
     }
-    
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        try{
+            if(e.getSource().equals(getView().btBus)){
+                model.Bus mBus=new model.Bus();
+                view.FrmBus vBus=new view.FrmBus();
+                controller.Bus cBus=new controller.Bus(mBus, vBus);
+                cBus.getView().setVisible(true);
+                this.getView().dispose();
+            }
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static void main(String[] args) {
+        model.Main mMain=new model.Main();
+        view.FrmDashboard vMain=new view.FrmDashboard();
+        controller.Main cMain=new controller.Main(mMain, vMain);
+        cMain.getView().setVisible(true);
+    }
+
+    public model.Main getModel() {
+        return model;
+    }
+
+    public void setModel(model.Main model) {
+        this.model = model;
+    }
 }
